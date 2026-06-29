cb7d751960ad0c72fd3ec575809d52deea908d91
Haroon <haroonshafiq25.insteva@gmail.com>
2026-06-29 17:15:33 +0500

    fix: remove CURRENT_DATE default for MySQL compatibility

--- database/migrations/2025_10_19_034915_create_coupons_table.php
+++ database/migrations/2025_10_19_034915_create_coupons_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-            Schema::create('coupons', function (Blueprint $table) {
+        Schema::create('coupons', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
-            $table->enum('type',['fixed', 'percent']);
+            $table->enum('type', ['fixed', 'percent']);
             $table->decimal('value');
             $table->decimal('cart_value');
-            $table->date('expiry_date')->default(DB::raw('CURRENT_DATE'));
+            $table->date('expiry_date')->nullable()->default(null);
             $table->timestamps();
         });
-
     }
 
     /**
